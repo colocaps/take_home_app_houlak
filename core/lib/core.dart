f9ba011decd6b3aea781739c3e014ce986178468
colocaps <colocaps@gmail.com>
2022-08-18 23:00:54 -0300

    added navigation manager

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,3 +1,6 @@
 library core;
 
+export 'package:core/src/abstractions/navigation_manager.dart'
+    show NavigationManager;
+
 export 'package:core/src/commons/application_theme.dart' show ApplicationTheme;
